167be0d8beadf5c0e4665210249967da9c7da811
Mathifaa <u20221a955@upc.edu.pe>
2024-10-23 22:35:28 -0500

    first commit

---
+++ src/main/java/com/lexmedd/backend/LexMeddApplication.java
@@ -0,0 +1,13 @@
+package com.lexmedd.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexMeddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexMeddApplication.class, args);
+	}
+
+}
