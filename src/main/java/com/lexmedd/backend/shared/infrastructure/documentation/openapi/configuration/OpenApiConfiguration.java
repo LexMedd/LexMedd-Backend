9f2716098662364828e2e2c3c225d6987ae452a7
Mathifa <u20221a955@upc.edu.pe>
2024-10-25 18:31:44 -0500

    feat : update documentaction

--- src/main/java/com/lexmedd/backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/lexmedd/backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -18,14 +18,14 @@ public class OpenApiConfiguration {
         var openApi = new OpenAPI();
         openApi
                 .info(new Info()
-                        .title("FromZero API")
-                        .description("FromZero application REST API documentation.")
+                        .title("Lexmedd API")
+                        .description("Lexmedd application REST API documentation.")
                         .version("v1.0.0")
                         .license(new License().name("Apache 2.0")
                                 .url("https://springdoc.org")))
                 .externalDocs(new ExternalDocumentation()
                         .description("FromZero wiki Documentation")
-                        .url("https://examify.wiki.github.io/docs"));
+                        .url("https://Lexmedd.wiki.github.io/docs"));
 
 
         // Add security scheme
